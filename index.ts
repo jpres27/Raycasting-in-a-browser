0e7bf36dd0df8b7a62a507be3538e475fd8a85c0
jpres27 <47310984+jpres27@users.noreply.github.com>
2024-10-18 01:33:19 -0400

    coloring sky and floor

--- index.ts
+++ index.ts
@@ -2,7 +2,9 @@ const EPSILON = 1e-6;
 const NEAR_PLANE = 0.1;
 const FAR_PLANE = 10.0;
 const FOV = Math.PI*0.5;
-const RAYS = 500;
+const SCREEN_FACTOR = 40;
+const VERT_RAYS = Math.floor(16*SCREEN_FACTOR);
+const HORZ_RAYS = Math.floor(9*SCREEN_FACTOR);
 const STEP_LENGTH = 0.3;
 
 class v2 {
@@ -111,12 +113,14 @@ class player {
     dPlayerP: v2;
     width: number;
     height: number;
+    wh: v2;
     constructor(position: v2, direction: number, dPlayerP: v2, width: number, height: number) {
         this.position = position;
         this.direction = direction;
         this.dPlayerP = dPlayerP;
         this.width = width;
         this.height = height;
+        this.wh = new v2(width, height);
     }
 }
 
@@ -151,7 +155,7 @@ function SnapFloorCeiling(x: number, dx: number): number {
     return x;
 }
 
-function HittingCell(p1: v2, p2: v2): v2 {
+function HittingTile(p1: v2, p2: v2): v2 {
     const d = p2.Subtract(p1);
 
     return new v2(Math.floor(p2.x + Math.sign(d.x)*EPSILON),
@@ -189,11 +193,12 @@ function RayStep(p1: v2, p2: v2): v2 {
 }
 
 class Level_Data {
-    cells: Array<number>;
+    tiles: Array<number>;
+    floor: number;
     width: number;
     height: number;
     wh: v2;
-    constructor(LevelArray: Array<Array<number>>) {
+    constructor(LevelArray: Array<Array<number>>, floor: number) {
         this.height = LevelArray.length;
         let x = Number.MIN_VALUE;
         for(let row of LevelArray) {
@@ -201,40 +206,48 @@ class Level_Data {
         }
         this.width = x;
         this.wh = new v2(this.width, this.height);
-        this.cells = [];
+        this.tiles = [];
         for(let row of LevelArray) {
-            this.cells = this.cells.concat(row);
+            this.tiles = this.tiles.concat(row);
             // NOTE: If rows are not of even length we will them out by adding 0 to pad
             for(let i = 0; i < this.width - row.length; ++i) {
-                this.cells.push(0);
+                this.tiles.push(0);
             }
         }
+        this.floor = floor;
     }
 
     Contains(p: v2): boolean {
         return (0 <= p.x) && (p.x < this.width) && (0 <= p.y) && (p.y < this.height);
     }
 
-    GetCell(p: v2): number | undefined {
+    GetTile(p: v2): number | undefined {
         if(!this.Contains(p)) {
             return undefined;
         } else {
             const floored_p = new v2(Math.floor(p.x), Math.floor(p.y));
-            return this.cells[(floored_p.y*this.width) + floored_p.x];
+            return this.tiles[(floored_p.y*this.width) + floored_p.x];
         }
     }
 
     IsWall(p: v2): boolean {
-        const Cell = this.GetCell(p);
-        return Cell!== 0 && Cell!== undefined; 
+        const Tile = this.GetTile(p);
+        return Tile!== 0 && Tile!== undefined; 
     }
+
+    GetFloor(p: v2): number | undefined {
+        if(!this.Contains(p)) return undefined;
+        // TODO: change when there is more than one floor texture
+        return this.floor;
+    }
+
 }
 
 function CastRay(Context: CanvasRenderingContext2D, LevelData : Level_Data, p1: v2, p2: v2): v2 {
     let Start = p1;
     while(Start.SqrDistanceTo(p1) < Context.canvas.height*Context.canvas.height) {
-        const cell = HittingCell(p1, p2);
-        if(LevelData.GetCell(cell) !== undefined && LevelData.GetCell(cell) !== 0) {
+        const cell = HittingTile(p1, p2);
+        if(LevelData.GetTile(cell) !== undefined && LevelData.GetTile(cell) !== 0) {
             break;
         }
         const p3 = RayStep(p1, p2);
@@ -260,7 +273,7 @@ function RenderMinimap(context: CanvasRenderingContext2D, Player: player,
     {
         for(let x = 0; x < grid_size.x; ++x) {
             const p = new v2(x, y);
-            if(LevelData.GetCell(p) !== 0) {
+            if(LevelData.GetTile(p) !== 0) {
                 context.fillStyle = "#0000A8";
                 context.fillRect(x, y, 1, 1);
             }
@@ -293,21 +306,26 @@ function RenderMinimap(context: CanvasRenderingContext2D, Player: player,
     context.restore();
 }
 
-function RenderGame(Context: CanvasRenderingContext2D, Player: player, LevelData: Level_Data, Textures: HTMLImageElement[]) {
-    const Strip_Width = Math.ceil(Context.canvas.width / RAYS);
+function RenderFloorCeiling(Context: CanvasRenderingContext2D, Player: player, LevelData: Level_Data, Textures: HTMLImageElement[]) {
+
+}
+
+function RenderWalls(Context: CanvasRenderingContext2D, Player: player, LevelData: Level_Data, Textures: HTMLImageElement[]) {
+    Context.save();
+    Context.scale(Context.canvas.width / VERT_RAYS, Context.canvas.height / HORZ_RAYS);
     const [r1, r2] = GetFOV(Player);
 
-    for (let x = 0; x < RAYS; ++x) {
-        const CollisionPoint = CastRay(Context, LevelData, Player.position, r1.Lerp(r2, x / RAYS));
-        const CollisionCell = HittingCell(Player.position, CollisionPoint);
-        const Cell = LevelData.GetCell(CollisionCell);
-        if (Cell !== 0 && Cell != undefined) {
+    for (let x = 0; x < VERT_RAYS; ++x) {
+        const CollisionPoint = CastRay(Context, LevelData, Player.position, r1.Lerp(r2, x / VERT_RAYS));
+        const CollisionTile = HittingTile(Player.position, CollisionPoint);
+        const Tile = LevelData.GetTile(CollisionTile);
+        if (Tile !== 0 && Tile != undefined) {
             const v = CollisionPoint.Subtract(Player.position);
             const d = v2.FromAngle(Player.direction);
             const PerpWallDist = v.Dot(d);
-            const Wall_Height = Context.canvas.height / PerpWallDist;
+            const StripHeight = HORZ_RAYS / PerpWallDist;
 
-            const t = CollisionPoint.Subtract(CollisionCell);
+            const t = CollisionPoint.Subtract(CollisionTile);
             let u = 0;
             if ((Math.abs(t.x) < EPSILON || Math.abs(t.x - 1) < EPSILON) && t.y > 0) {
                 u = t.y;
@@ -315,42 +333,49 @@ function RenderGame(Context: CanvasRenderingContext2D, Player: player, LevelData
                 u = t.x;
             }
 
-            switch (Cell) {
+            let DrawTexture: HTMLImageElement | undefined;
+            switch (Tile) {
                 case 1: {
-                    // Context.fillStyle = new rgba(3, 99, 52, 1).Brightness(1/PerpWallDist).String();
-                    Context.drawImage(Textures[0], u * Textures[0].width, 0, 1, Textures[0].height,
-                        x * Strip_Width, (Context.canvas.height - Wall_Height) * 0.5, Strip_Width, Wall_Height);
+                    DrawTexture = Textures[0];
                 } break;
                 case 2: {
-                    // Context.fillStyle = new rgba(34, 102,195, 1).Brightness(1/PerpWallDist).String();
-                    Context.drawImage(Textures[1], u * Textures[1].width, 0, 1, Textures[1].height,
-                        x * Strip_Width, (Context.canvas.height - Wall_Height) * 0.5, Strip_Width, Wall_Height);
+                    DrawTexture = Textures[1];
                 } break;
                 case 3: {
-                    // Context.fillStyle = new rgba(221, 149, 68, 1).Brightness(1/PerpWallDist).String();
-                    Context.drawImage(Textures[2], u * Textures[2].width, 0, 1, Textures[2].height,
-                        x * Strip_Width, (Context.canvas.height - Wall_Height) * 0.5, Strip_Width, Wall_Height);
+                    DrawTexture = Textures[2];
                 } break;
                 case 4: {
-                    // Context.fillStyle = new rgba(0, 12, 101, 1).Brightness(1/PerpWallDist).String();
-                    Context.drawImage(Textures[3], u * Textures[3].width, 0, 1, Textures[3].height,
-                        x * Strip_Width, (Context.canvas.height - Wall_Height) * 0.5, Strip_Width, Wall_Height);
+                    DrawTexture = Textures[3];
                 } break;
             }
             // NOTE: Code that was being used to draw plain colors
-            // Context.fillRect(x*Strip_Width, (Context.canvas.height - Wall_Height)*0.5, Strip_Width, Wall_Height);
+            // Context.fillRect(x*PixelWidth, (HORZ_RAYS - StripHeight)*0.5, PixelWidth, StripHeight);
+
+            if(DrawTexture !== undefined) { 
+                Context.drawImage(DrawTexture, Math.floor(u*DrawTexture.width), 0, 1, DrawTexture.height,
+                    Math.floor(x), Math.floor((HORZ_RAYS - StripHeight)*0.5), 
+                    Math.ceil(1), Math.ceil(StripHeight));
+                Context.fillStyle = new rgba(0, 0, 0, 1-1/PerpWallDist).String();
+                Context.fillRect(Math.floor(x), Math.floor((HORZ_RAYS - StripHeight)*0.5), 
+                    Math.ceil(1), Math.ceil(StripHeight));
+            }
         }
     }
+    Context.restore();
 }
 
-function Render(Context: CanvasRenderingContext2D, Player: player, LevelData: Level_Data, Textures: HTMLImageElement[]) {
+function Render(Context: CanvasRenderingContext2D, Player: player, LevelData: Level_Data, Textures: HTMLImageElement[], DisplayMap: boolean) {
     const Minimap_Pos = v2.Zero().Add(GetCanvasSize(Context).Scale(0.03));
     const Cell_Size = Context.canvas.width*0.02;
     const Minimap_Size = LevelData.wh.Scale(Cell_Size);
-    Context.fillStyle = "#181818";
+    Context.fillStyle = "#3D465B";
     Context.fillRect(0, 0, Context.canvas.width, Context.canvas.height);
-    RenderGame(Context, Player, LevelData, Textures)
-    RenderMinimap(Context, Player, Minimap_Pos, Minimap_Size, LevelData);
+    Context.fillStyle = "#282828";
+    Context.fillRect(0, Context.canvas.height/2, Context.canvas.width, Context.canvas.height/2);
+    RenderWalls(Context, Player, LevelData, Textures)
+    if(DisplayMap) {
+        RenderMinimap(Context, Player, Minimap_Pos, Minimap_Size, LevelData);
+    }
 }
 
 async function LoadImg(url: string): Promise<HTMLImageElement> {
@@ -361,29 +386,51 @@ async function LoadImg(url: string): Promise<HTMLImageElement> {
         Img.onload = () => resolve(Img);
         Img.onerror = reject;
     });
+}
 
+function TestWall(LevelData: Level_Data, Player: player, NewPlayerP: v2): boolean {
+    let hit = false;
+    const Corner = NewPlayerP.Subtract(Player.wh.Scale(0.5));
+        
+    for(let x = 0; x < 2; ++x) {
+        for(let y = 0; y < 2; ++y) {
+            if((LevelData.GetTile(Corner.Add(new v2(x, y)).Scale(Player.width))) !== 0) {
+                hit = true;
+            }
+        }
+    }
+    return hit;
 }
 
 (async () => {
     
-    const tex01 = await LoadImg("./textures/brick25.png");
-    const tex02 = await LoadImg("./textures/tile15.png");
-    const tex03 = await LoadImg("./textures/tile16.png");
+    const tex01 = await LoadImg("./textures/Cobble_Wall.png");
+    const tex02 = await LoadImg("./textures/stone7.png");
+    const tex03 = await LoadImg("./textures/grass2.png");
     const tex04 = await LoadImg("./textures/tile24.png");
     let Textures = [tex01, tex02, tex03, tex04];
 
     let Cells = [
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 2, 0, 0, 0, 2, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 3, 0, 0, 0, 4, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 2],
+        [1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 2],
+        [1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 2],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2],
+        [1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 2],
+        [1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 2],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 2],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2],
     ];
 
-    let LevelData = new Level_Data(Cells);
+    let LevelData = new Level_Data(Cells, 1);
 
     const Game = document.getElementById("game") as (HTMLCanvasElement | null);
     if(Game === null) {
@@ -399,12 +446,13 @@ async function LoadImg(url: string): Promise<HTMLImageElement> {
             throw new Error("2d context is null");
     }
 
-    let Player = new player(LevelData.wh.Multiply(new v2(0.83, 0.73)), Math.PI*1.25, v2.Zero(), 0.2, 0.2);
+    let Player = new player(LevelData.wh.Multiply(new v2(0.83, 0.73)), Math.PI*1.25, v2.Zero(), 0.5, 0.5);
 
     let MovingFwd = false;
     let MovingBwd = false;
     let TurnLeft = false;
     let TurnRight = false;
+    let DisplayMap = false;
 
     window.addEventListener("keydown", (e) => {
         switch (e.code) {
@@ -420,6 +468,9 @@ async function LoadImg(url: string): Promise<HTMLImageElement> {
             case 'KeyA': {
                 TurnLeft = true;
             } break;
+            case 'KeyM': {
+                DisplayMap = !DisplayMap
+            }
         }
     })
 
@@ -474,13 +525,13 @@ async function LoadImg(url: string): Promise<HTMLImageElement> {
         const PlayerDelta = ddPlayerP.Scale(0.5).Scale(dtForFrame*dtForFrame).Add(Player.dPlayerP.Scale(dtForFrame));
         Player.dPlayerP = Player.dPlayerP.Add(ddPlayerP.Scale(dtForFrame));
         const NewPlayerP = OldPlayerP.Add(PlayerDelta);
-
         Player.direction = Player.direction + AngularVelocity*dtForFrame;
-        
-        if(LevelData.GetCell(NewPlayerP) === 0){
+
+        if(LevelData.GetTile(NewPlayerP) === 0){
             Player.position = NewPlayerP;
         }
-        Render(Context, Player, LevelData, Textures);
+
+        Render(Context, Player, LevelData, Textures, DisplayMap);
         window.requestAnimationFrame(frame);
     }
     window.requestAnimationFrame((Time) => {
